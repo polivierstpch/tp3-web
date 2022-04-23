0744b05630902bd4d981cddc80dd627a30d87031
Caroline Marissal-Rousseau <1233968@cegeplimoilou.ca>
2022-04-23 18:35:33 -0400

    Ajout base participation

---
+++ Web2.UI/src/components/ParticipationForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="$emit('submit', formParticipation)">
+    <div>
+        <label for="courriel">Adresse courriel</label>
+        <input type="text" name="courriel" placeholder="participer@eventtracker.com" v-model="formParticipation.courriel" :disabled="!isEditing"/>
+    </div>
+    <div>
+        <label for="prenom">Prénom</label>
+        <input type="text" name="prenom" placeholder="John" v-model="formParticipation.prenom" :disabled="!isEditing"/>
+    </div>
+    <div>
+        <label for="Nom">Nom</label>
+        <input type="text" name="Nom" placeholder="Doe" v-model="formParticipation.Nom" :disabled="!isEditing"/>
+    </div>
+        <div>
+        <label for="nbPlace">Nombre de place</label>
+        <input type="number" name="nbPlace" placeholder="1" min="1" max="10" v-model.number="formParticipation.nbPlace" :disabled="!isEditing"/>
+    </div>
+     <input type="submit" value="Réserver" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ParticipationForm",
+  props: {
+    participation: {
+      type: Object,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      formParticipation: this.props.event
+    };
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  emits: ["submit"]
+};
+</script> 
---
+++ Web2.UI/src/views/ParticipationsView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="content">
+      <h1>Réservez vos places</h1>
+      <h3>
+        Prenez place à une expérience inoubliable avec  <img src="../assets/LogoEventTracker.png" alt="Logo Event Tracker">
+      </h3>
+     <ParticipationsForm @submit="ajouterParticipation"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  methods: {
+      ajouterParticipation(participation) {
+          const payload = { eventId: $route.params.id, participation };
+          this.$store.dispatch("ajouterParticipation", payload);
+      }
+  }
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  text-align: justify;
+}
+
+.content ul {
+  padding: 0 1em;
+}
+
+.content img {
+  width: 200px;
+}
+</style>
